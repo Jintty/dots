ee114e4be0189442eb1db8244d6c4c2c9e4908dc
Jintty <re4urnzer0@outlook.com>
2026-02-26 00:50:01 +0800

    update

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -7,18 +7,3 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.list = false
 vim.opt.scrolloff = 999
 vim.opt.sidescrolloff = 15
-
-if vim.g.neovide then
-  vim.g.neovide_opacity = 0.9
-  vim.g.neovide_normal_opacity = 0.9
-  vim.g.neovide_theme = "auto"
-  vim.g.neovide_remember_window_size = true
-  vim.g.neovide_fullscreen = true
-  vim.g.neovide_cursor_vfx_mode = "pixiedust"
-  vim.g.neovide_hide_mouse_when_typing = true
-
-  vim.g.neovide_title_background_color =
-    string.format("%x", vim.api.nvim_get_hl(0, { id = vim.api.nvim_get_hl_id_by_name("Normal") }).bg)
-
-  vim.g.neovide_title_text_color = "pink"
-end
